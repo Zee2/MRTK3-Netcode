404c065d8cca667e4d3f0ce9fd744db19e56a854
Finn Sinclair <finnnorth@gmail.com>
2022-08-22 18:28:42 -0700

    test

--- Assets/Scripts/NetworkCommandLine.cs
+++ Assets/Scripts/NetworkCommandLine.cs
@@ -1,5 +1,5 @@
 // From: https://docs-multiplayer.unity3d.com/netcode/current/tutorials/helloworld
-// Testtesttest
+// Testtesttesttest
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
